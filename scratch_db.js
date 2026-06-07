9b948e63b49a397444058304794c38c571ccd900
HangNguyen157 <nguyenthithuyhang1572006@gmail.com>
2026-06-08 01:52:13 +0700

    chore: remove debug files

--- scratch_db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '2542006',
-  database: 'edu_forum',
-});
